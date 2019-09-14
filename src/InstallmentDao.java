2c5da82ae39e5a46a55f1c04eac2863dd5701c97
12096316 <sagar.dawase@cqumail.com>
2019-09-14 15:39:18 +1000

    Name change

--- src/InstallmentDao.java
+++ src/InstallmentDao.java
@@ -2,12 +2,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class InstallmentDao {
-public static int save(String technician,String address,String hp,String zones,String outlet, String start, String end){
+public static int save(String name,String address,String hp,String zones,String outlet, String start, String end){
 	int status=0;
 	try{
 		Connection con=DB.getConnection();
-		PreparedStatement ps=con.prepareStatement("insert into installation(technician,address,hp,zones,outlet, start, end) values(?,?,?,?,?,?,?)");
-		ps.setString(1,technician);
+		PreparedStatement ps=con.prepareStatement("insert into installation(name,address,hp,zones,outlet, start, end) values(?,?,?,?,?,?,?)");
+		ps.setString(1,name);
 		ps.setString(2,address);
 		ps.setString(3,hp);
 		ps.setString(4,zones);
